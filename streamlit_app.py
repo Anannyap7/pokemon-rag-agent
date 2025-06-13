d88ef57a6e0e27c11ecb137b8e5832458ec1eb6a
Anannya Popat <59221653+Anannyap7@users.noreply.github.com>
2025-06-13 17:11:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,26 @@
 import streamlit as st
+from dotenv import load_dotenv
+
+# LangChain imports
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.tools import tool
+from langchain.memory import ConversationBufferMemory
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+
+load_dotenv()
 
 st.title('👾 Pokemon Battle Analyzer')
 st.info('This app builds a RAG agent for Pokemon Battle Analysis!')
-st.write('Hello world!')
+
+with st.expander('Dataset'):
+  st.write('**Raw Data**')
+
+  # Load the transcript
+  document = []
+  doc = TextLoader("pokemon_transcript.txt").load()
+  document.extend(doc)
+  document
