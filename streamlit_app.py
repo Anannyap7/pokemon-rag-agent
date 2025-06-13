edc6159429b0d05602805102c9acfedae46e610c
Anannya Popat <59221653+Anannyap7@users.noreply.github.com>
2025-06-13 18:06:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 
 # Fix protobuf compatibility issue
+'''
+This environment variable tells the protobuf library to use the pure Python implementation instead of the C++ implementation
+'''
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
 load_dotenv()
